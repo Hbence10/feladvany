391bea0aef7d4fe9f11f4542aaa1f0656de44a08
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-26 20:13:35 +0100

    Product class tulajdonságainak felvétele

--- backend/src/main/java/com/example/demo/model/Product.java
+++ backend/src/main/java/com/example/demo/model/Product.java
@@ -1,4 +1,27 @@
 package com.example.demo.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Product {
+
+    private Integer id;
+    private String productName;
+    private Integer grossPriceHuf;
+    private Integer netPriceHuf;
+    private String currency;
+    private Double vatRate;
+    private Integer quantityAvailable;
+    private Integer stockQuantity;
+    private Integer brand;
+    private Integer ean;
+    private LocalDateTime updatedAt;
 }
