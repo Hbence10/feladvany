7f23ea5a0563840344b53240ac423d96ba7c34ba
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-26 20:26:45 +0100

    angular projekt alapjainak megirasa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "homePage", component: HomePage, title: "Kezdőlap" },
+  { path: "", pathMatch: "full", redirectTo: "homePage" },
+  { path: "**", component: NotFound }
+];
