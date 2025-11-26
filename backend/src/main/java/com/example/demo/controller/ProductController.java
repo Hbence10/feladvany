c6d3219bc49713952c136a89b472c0c478060d51
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-26 19:25:55 +0100

    service és controller alapjainak megírása

--- backend/src/main/java/com/example/demo/controller/ProductController.java
+++ backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("")
+    public ResponseEntity<Object> getProducts(@RequestParam("filter") String filter, @RequestParam("sort") String sort, @RequestParam("onlyValid") String onlyValid){
+        return productService.getProducts(filter, sort, onlyValid);
+    }
 }
--- backend/src/main/java/com/example/demo/service/ProductService.java
+++ backend/src/main/java/com/example/demo/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.example.demo.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ProductService {
+
+    public ResponseEntity<Object> getProducts(String filter, String sort, String onlyValid){
+        return null;
+    }
 }
