49b6dc7370f93cbbac8a12e669961aa6a08221e5
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-27 18:00:15 +0100

    card body 1.0

--- frontend/src/app/components/product-card/product-card.ts
+++ frontend/src/app/components/product-card/product-card.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, CommonModule],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
