07c39d86f18d2b1353754954383b0f84856da889
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-29 12:47:14 +0100

    maradék dokumentáció

--- backend/src/main/java/com/example/demo/controller/ProductController.java
+++ backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,8 +25,13 @@ public class ProductController {
 
     @Operation(summary = "Termékek kilistázása", description = "Az adott kritériumok alapján visszaadja a termékeket.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Sikeres termék lista lekérdezés."),
-            @ApiResponse(responseCode = "404", description = "Nem megfelelő filter vagy sort tipus.")
+            @ApiResponse(responseCode = "200", description = "Sikeres termék lista lekérdezés.", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Nem megfelelő filter vagy sort tipus.", useReturnTypeSchema = false)
+    })
+    @Parameters({
+            @Parameter(name = "filter", allowEmptyValue = true, description = "Az adott rendezés tipusa. 3 fajta lehet csak: name, netPrice vagy grossPrice."),
+            @Parameter(name = "sort", allowEmptyValue = true, description = "Az adott rendezés sorrendjét ábrázolja. 2 fajta lehet csak: ASC (növekvő) vagy DESC (csökkenő)."),
+            @Parameter(name = "onlyValid", allowEmptyValue = true, description = "Azt adja vissza, hogy érvényes vagy nem érvényes termékek jönnek vissza. 2 fajta lehet csak: true (csak érvényes) vagy false (nem csak érvényes)."),
     })
     @GetMapping("")
     public ResponseEntity<List<Product>> getProducts(@RequestParam(name = "filter", defaultValue = "name") String filter, @RequestParam(name = "sort", defaultValue = "ASC") String sort, @RequestParam(name = "onlyValid", defaultValue = "false") String onlyValid) {
