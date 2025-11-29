582cb2d2209d6682b258f4ebcd90b6cf2a96745b
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-29 12:25:10 +0100

    + információ hozzáadása

--- backend/src/main/java/com/example/demo/controller/ProductController.java
+++ backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,10 @@ public class ProductController {
     private final ProductService productService;
 
     @Operation(summary = "Termékek kilistázása", description = "Az adott kritériumok alapján visszaadja a termékeket.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sikeres termék lista lekérdezés."),
+            @ApiResponse(responseCode = "404", description = "Nem megfelelő filter vagy sort tipus.")
+    })
     @GetMapping("")
     public ResponseEntity<List<Product>> getProducts(@RequestParam(name = "filter", defaultValue = "name") String filter, @RequestParam(name = "sort", defaultValue = "ASC") String sort, @RequestParam(name = "onlyValid", defaultValue = "false") String onlyValid) {
         return productService.getProducts(filter, sort, onlyValid.equals("true"));
